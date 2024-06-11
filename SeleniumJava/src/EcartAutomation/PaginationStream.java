b0bdd0ec76ad7c939299ff5adc8aaa1dbc6f5318
Kapil Kakade <kapilkak@amdocs.com>
2024-06-11 23:15:18 +0530

    filter and pagination

---
+++ SeleniumJava/src/EcartAutomation/PaginationStream.java
@@ -0,0 +1,10 @@
+package EcartAutomation;
+
+public class PaginationStream {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ SeleniumJava/src/EcartAutomation/filterStream.java
@@ -0,0 +1,10 @@
+package EcartAutomation;
+
+public class filterStream {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
